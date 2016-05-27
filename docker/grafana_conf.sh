9727da084f1d0dac82fead5be430a4b90ae221d8
Marc Grunberg <marc.grunberg@unistra.fr>
2016-05-27 13:36:10 +0200

    automaticaly add influxdb datasource

---
+++ docker/grafana_conf.sh
@@ -0,0 +1,11 @@
+curl 'http://admin:admin@192.168.99.100:3000/api/datasources' \
+    -X POST \
+    -H 'Content-Type: application/json;charset=UTF-8' \
+    --data-binary '{"name":"influxdb",
+                    "type":"influxdb",
+                    "url":"http://192.168.99.100:8086",
+                    "access":"proxy",
+                    "isDefault":true,
+                    "database":"eost",
+                    "user":"admin",
+                    "password":"admin"}'
