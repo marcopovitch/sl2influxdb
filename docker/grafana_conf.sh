ea00d0ba4b787fc30e938441022b21a0365e677d
Marc Grunberg <marc.grunberg@unistra.fr>
2017-01-24 18:50:46 +0100

    s/ip/localhost/g

--- docker/grafana_conf.sh
+++ docker/grafana_conf.sh
@@ -1,10 +1,10 @@
-curl 'http://admin:admin@192.168.99.100:3000/api/datasources' \
+curl 'http://admin:admin@localhost:3000/api/datasources' \
     -X POST \
     -H 'Content-Type: application/json;charset=UTF-8' \
     --data-binary '{"name":"influxdb",
                     "type":"influxdb",
-                    "url":"http://192.168.99.100:8086",
-                    "access":"proxy",
+                    "url":"http://localhost:8086",
+                    "access":"direct",
                     "isDefault":true,
                     "database":"eost",
                     "user":"admin",
