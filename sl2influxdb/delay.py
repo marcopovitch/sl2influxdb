353e4ee5e98076bf73112c6e218bbd7273541d6a
Marc Grunberg <marc.grunberg@unistra.fr>
2022-03-08 00:37:55 +0100

    lock outside for loop

--- sl2influxdb/delay.py
+++ sl2influxdb/delay.py
@@ -12,23 +12,20 @@ from sl2influxdb.threads import shutdown_event, lock
 from sl2influxdb.threads import last_packet_time
 
 # default logger
-logger = logging.getLogger('DelayInfluxDBExporter')
+logger = logging.getLogger("DelayInfluxDBExporter")
 
 
 class LatencyDelayInfluxDBExporter(InfluxDBExporter):
-    def __init__(self, host, port,
-                 dbname, user, pwd,
-                 flushtime,
-                 dropdb=False,
-                 geohash={}):
-        super(LatencyDelayInfluxDBExporter, self).__init__(host, port,
-                                                           dbname, user, pwd,
-                                                           flushtime,
-                                                           dropdb, geohash)
-        self.refresh_rate = 1.  # sec.
+    def __init__(
+        self, host, port, dbname, user, pwd, flushtime, dropdb=False, geohash={}
+    ):
+        super(LatencyDelayInfluxDBExporter, self).__init__(
+            host, port, dbname, user, pwd, flushtime, dropdb, geohash
+        )
+        self.refresh_rate = 1.0  # sec.
 
     def make_channel_latency_delay(self, channel, last_packet):
-        """ Latency and Delay computation
+        """Latency and Delay computation
 
         Latency is the elapsed time between the current time and
         the time of the last sample from the last received packet.
@@ -41,38 +38,44 @@ class LatencyDelayInfluxDBExporter(InfluxDBExporter):
         (http://www.seiscomp3.org/doc/jakarta/current/apps/scqc.html)
         """
         now = datetime.utcnow()
-        delay = UTCDateTime(now) - last_packet['timestamp']
-        latency = UTCDateTime(now) - last_packet['endtime']
+        delay = UTCDateTime(now) - last_packet["timestamp"]
+        latency = UTCDateTime(now) - last_packet["endtime"]
 
         try:
             geohash_tag = ",geohash=%s" % self.geohash[channel]
         except:
             geohash_tag = ""
 
-        t = timegm(now.utctimetuple()) * 1e9 \
-            + now.microsecond * 1e3
+        t = timegm(now.utctimetuple()) * 1e9 + now.microsecond * 1e3
         t_str = str(int(t))
 
         # delay
-        s = "delay,channel=%s" % channel + \
-            geohash_tag + \
-            " value=%.2f " % delay + \
-            "%s" % t_str
+        s = (
+            "delay,channel=%s" % channel
+            + geohash_tag
+            + " value=%.2f " % delay
+            + "%s" % t_str
+        )
         self.data.append(s)
 
         # latency
-        l = "latency,channel=" + channel + \
-            geohash_tag + \
-            " value=" + "%.1f " % latency + \
-            str(int(t))
+        l = (
+            "latency,channel="
+            + channel
+            + geohash_tag
+            + " value="
+            + "%.1f " % latency
+            + str(int(t))
+        )
         self.data.append(l)
 
     def manage_data(self):
-        """ Prepare data to be sent to influxdb. """
+        """Prepare data to be sent to influxdb."""
+        lock.acquire()
         for c in last_packet_time.keys():
-            lock.acquire()
             self.make_channel_latency_delay(c, last_packet_time[c])
-            lock.release()
+        lock.release()
+
         try:
             self.send_points()
         except BaseException as e:
@@ -81,11 +84,12 @@ class LatencyDelayInfluxDBExporter(InfluxDBExporter):
         return True
 
     def run(self):
-        """ Main loop. """
+        """Main loop."""
         while True:
             self.manage_data()
             shutdown_event.wait(self.refresh_rate)
             if shutdown_event.isSet():
-                logger.info("%s thread has catched *shutdown_event*" %
-                            self.__class__.__name__)
+                logger.info(
+                    "%s thread has catched *shutdown_event*" % self.__class__.__name__
+                )
                 sys.exit(0)
